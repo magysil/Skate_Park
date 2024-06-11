5f1095379d4f254f9b025fcd8948ed8168074470
magysil <magallysilvab@gmail.com>
2024-06-11 18:17:46 -0400

    actulizaciòn de la config de las tablas

--- config/tablas.sql
+++ config/tablas.sql
@@ -4,16 +4,31 @@ CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
 CREATE TABLE skaters (
 	id SERIAL Primary key, 
-	email VARCHAR(50) NOT NULL, 
+	email VARCHAR(50) NOT NULL unique, 
 	nombre VARCHAR(25) NOT NULL, 
-	text NOT NULL, 
+	password text NOT NULL, 
 	anos_experiencia INT NOT NULL, 
 	especialidad VARCHAR(50) NOT NULL, 
 	foto VARCHAR(255) NOT NULL, 
-	estado BOOLEAN NOT NULL
+	estado BOOLEAN NOT NULL,
+	rol VARCHAR (25) NOT NULL DEFAULT 'Usuario' 
 );
+
+/* Añadir Usuario con rol administrador en la base de datos*/
+INSERT INTO skaters (email, nombre, password, anos_experiencia, especialidad, foto, estado, rol)
+VALUES (
+    'admin@gmail.com', 
+    'AdminUser', 
+    crypt('securepassword', gen_salt('bf')), 
+    10, 
+    'Manager', 
+    'admin_photo_url', 
+    TRUE, 
+    'Administrador'
+);
+
 --------------------------------------------------------------------------------------------------------------------------------------------------
-/*Ejemplo de .env
+/*Ejemplo de .env*/
 
 PORT = ****
 
